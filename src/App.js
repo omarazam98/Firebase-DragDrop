e4c350ccc804f933583e4b8e80a2d303749a07a5
Brendan Lucas <brendanwflucas@gmail.com>
2019-04-11 13:46:55 -0400

    Added config files and basic react app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    data: null
+  };
+  componentDidMount() {
+    // Call our fetch function below once the component mounts
+    this.callBackendAPI()
+        .then(res => this.setState({data: res.express}))
+        .catch(err => console.log(err));
+  }
+
+  callBackendAPI = async () => {
+    const response = await fetch('/winwin_homesharing');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message)
+    }
+    return body;
+  };
+
   render() {
     return (
       <div className="App">
@@ -20,6 +40,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <p className="App-intro">{this.state.data}</p>
       </div>
     );
   }
