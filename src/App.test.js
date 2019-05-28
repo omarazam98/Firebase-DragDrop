9487d2b5b7d3339e7d432906a22cda83b22f11a6
Julia <julia.st.jean@winwinhomesharing.com>
2019-05-28 12:38:15 -0400

    Added render test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import renderer from 'react-test-renderer';
+import UploadPage from './components/Upload';
 
-it('renders without crashing', () => {
+it('renders App without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App/>, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+jest.mock('./components/Upload', () => () => 'Upload');
+
+test('UploadPage renders', () => {
+  const component = renderer.create(
+      <UploadPage/>,
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
