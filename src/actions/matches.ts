ea8c7aa79b0f90e586c3a44a96e824da599945dd
Mattias Lightstone <matt1light@gmail.com>
2019-06-06 11:52:47 -0400

    Add asynchronous actions to matches
    Includes startSetMatches
    startDeclineMatch
    startAcceptMatch

--- src/actions/matches.ts
+++ src/actions/matches.ts
@@ -3,7 +3,7 @@ export const DECLINE_MATCH_NAME = 'DECLINE_MATCH';
 export const SET_MATCHES_NAME = 'SET_MATCHES';
 
 // ACCEPT_MATCH
-export const acceptMatch = (id:string):MatchesActionType => {
+export const acceptMatch = (id:string) => {
   return {
     id,
     type: ACCEPT_MATCH_NAME,
@@ -11,7 +11,7 @@ export const acceptMatch = (id:string):MatchesActionType => {
 };
 
 // DECLINE_MATCH
-export const declineMatch = (id:string):MatchesActionType => {
+export const declineMatch = (id:string) => {
   return {
     id,
     type: DECLINE_MATCH_NAME,
@@ -19,9 +19,39 @@ export const declineMatch = (id:string):MatchesActionType => {
 };
 
 // SET_MATCHES
-export const setMatches = (matches:MatchType[]):MatchesActionType => {
+export const setMatches = (matches:MatchType[]) => {
   return {
     matches,
     type: SET_MATCHES_NAME,
+  };
+};
+
+// START_SET_MATCHES
+// This is an asynchronous thunk action
+// it fetches the matches of a certain student and then call set matches with those matches
+export const startSetMatches = (api, studentId:string) => {
+  return (dispatch) => {
+    // This return is required in order to use the promise in the calling dispatch
+    return api.fetchMatchesByStudent(studentId).then((matches) => {
+      dispatch(setMatches(matches));
+    });
+  };
+};
+
+export const startAcceptMatch = (api, studentId:string, matchId:string) => {
+  return (dispatch) => {
+    return api.updateMatchByStudentAndId(studentId, matchId, { match_state: 'accepted' })
+      .then((result) => {
+        dispatch(acceptMatch(matchId));
+      });
+  };
+};
+
+export const startDeclineMatch = (api, studentId:string, matchId:string) => {
+  return (dispatch) => {
+    return api.updateMatchByStudentAndId(studentId, matchId, { match_state: 'declined' })
+      .then((result) => {
+        dispatch(declineMatch(matchId));
+      });
   };
 };
