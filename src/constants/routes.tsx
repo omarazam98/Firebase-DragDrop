64692dc654414933be066b00c981b3c1652726d7
Julia <julia.st.jean@winwinhomesharing.com>
2019-06-16 22:25:14 -0400

    Added session persistence option for log in

--- src/constants/routes.tsx
+++ src/constants/routes.tsx
@@ -2,7 +2,7 @@
 // each page/component should have a route
 // e.g. winwinhomesharing.org/signup would have the route 'signup'
 import React from 'react';
-import Login from '../components/Login/Login'
+import Login from '../components/Login/Login';
 
 // to add a new page, simply add another element to the array below
 // path is the path to the page
