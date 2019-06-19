81816469b8650fcb1fb117c63b644b9d67ed5757
Julia <julia.st.jean@winwinhomesharing.com>
2019-06-18 20:48:21 -0400

    Fixed log out test

--- src/tests/components/Navigation.test.tsx
+++ src/tests/components/Navigation.test.tsx
@@ -1,27 +1,27 @@
 import  { Navigation }from '../../components/Navigation';
 import * as React from 'react';
 import Enzyme, { shallow, render, mount } from 'enzyme/build';
-import mock = jest.mock;
 
 const options = {
   lifecycleExperimental: false,
   disableLifecycleMethods: true,
 };
+
 const mockAPI = {
   auth: {
     firebaseAuth: {
       signOut: jest.fn(),
     }
   }
-}
+};
+
 test('Render the navigation', () => {
   const test = <Navigation />;
   const wrapper = shallow(test, options);
   expect(wrapper).toMatchSnapshot();
 });
 
-
-test('Sign out button logs out user', () => {
+test('Sign out button calls sign out', () => {
   const test = <Navigation api={ mockAPI } />;
   const wrapper = shallow(test, options);
   wrapper.find('#signOut').simulate('click');
