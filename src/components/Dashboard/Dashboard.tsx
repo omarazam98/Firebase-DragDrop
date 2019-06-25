dee7d46fa8db98e10eb13b93e5b684f7c90f6af9
Julia <julia.st.jean@winwinhomesharing.com>
2019-06-25 15:43:41 -0400

    Removed firebaseAuth reference

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -2,15 +2,10 @@ import React from 'react';
 import {withAPI} from '@winwin/api-firebase';
 
 export function Dashboard(props) {
-    if (props.api.auth.currentUser) {
-        return (
-            <div>Dashboard</div>
-        );
-    }else{
-        return (
-            <div>Not Authorized</div>
-        )
-    }
+    return (
+        <div>Dashboard</div>
+    )
+
 }
 
 export default withAPI(Dashboard);
