e03a6fc61c49a43cbb8f6b4643468e44e7f07eab
Julia <julia.st.jean@winwinhomesharing.com>
2019-06-25 20:10:40 -0400

    Added sample card

--- src/components/PaymentCard/index.tsx
+++ src/components/PaymentCard/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+<<<<<<< refs/remotes/origin/integration
 import { withAPI } from '@winwin/api-firebase';
 import { Card } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
@@ -121,10 +122,41 @@ export class PaymentCard extends React.Component<PaymentProps, PaymentState>{
             <Button className={this.props.classes.changeButton} variant="contained">Change</Button>
           </CardActions>
         </Card>
+=======
+import './index.css';
+
+export class PaymentCard extends React.Component<any, any>{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <div className='credit-card'>
+        <div className='credit-card__logo'>Logo</div>
+
+        <div className='credit-card__number'>XXXX XXXX XXXX 2516</div>
+
+        <div className='credit-card__info'>
+          <div className='credit-card__info_name'>
+            <div className='credit-card__info_label'>CARDHOLDER'S NAME</div>
+            <div>MATT SMITH</div>
+          </div>
+
+          <div className='credit-card__info_expiry'>
+            <div className='credit-card__info_label'>VALID UP TO</div>
+            <div>06/2027</div>
+          </div>
+        </div>
+
+>>>>>>> Added sample card
       </div>
     );
   }
 }
 
+<<<<<<< refs/remotes/origin/integration
 export const PaymentWithStyle = withStyles(styles)(PaymentCard);
 export default (withAPI(PaymentWithStyle));
+=======
+export default PaymentCard;
+>>>>>>> Added sample card
