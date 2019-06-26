7a873326d5d313642b438e3c499be49c77bb514a
Julia <julia.st.jean@winwinhomesharing.com>
2019-06-26 11:21:57 -0400

    Pulling payment methods from API

--- src/components/PaymentCard/index.tsx
+++ src/components/PaymentCard/index.tsx
@@ -126,24 +126,57 @@ export class PaymentCard extends React.Component<PaymentProps, PaymentState>{
 import './index.css';
 import { withAPI } from '@winwin/api-firebase';
 
-export class PaymentCard extends React.Component<any, any>{
+interface PaymentMethods {
+  type: string,
+  number: number,
+  main: boolean,
+}
+
+interface UserType {
+  displayName: string,
+  uid: string,
+}
+
+interface PaymentState {
+  paymentMethods: PaymentMethods[] | undefined;
+  user: UserType | undefined;
+}
+const INITIAL_STATE = {
+  paymentMethods: undefined,
+  user: undefined,
+}
+export class PaymentCard extends React.Component<any,PaymentState>{
   constructor(props){
     super(props);
+    this.state = INITIAL_STATE;
   }
-  render(){
-    if (this.props.api.auth.currentUser()) {
-      console.log(this.props.api.auth.currentUser())
+
+  componentWillMount() {
+    const user = this.props.api.auth.currentUser();
+    if (user) {
+      this.props.api.data.users.getPaymentMethods(user.uid).then((payments) => {
+        this.setState(() => {
+          return {
+            paymentMethods: payments,
+            user,
+          }
+        })
+      });
     }
+  }
+
+  render(){
     return(
       <div className='credit-card'>
-        <div className='credit-card__logo'>Logo</div>
-
-        <div className='credit-card__number'>XXXX XXXX XXXX 2516</div>
+        <div className='credit-card__logo'>Win-Win</div>
+        <div className='credit-card__number'>
+          {this.state.paymentMethods && "XXXX XXXX XXXX " + this.state.paymentMethods[0].number}
+        </div>
 
         <div className='credit-card__info'>
           <div className='credit-card__info_name'>
             <div className='credit-card__info_label'>CARDHOLDER'S NAME</div>
-            <div>{this.props.api.auth.currentUser() && this.props.api.data.users.getName()}</div>
+            <div>{this.state.user && this.state.user.displayName}</div>
           </div>
 
           <div className='credit-card__info_expiry'>
