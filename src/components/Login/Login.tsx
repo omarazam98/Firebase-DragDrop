06968ca9f910363e86ead0b32728c601c64aad11
Brendan Lucas <brendanwflucas@gmail.com>
2019-06-26 14:18:01 -0400

    fixed tslinter errors

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -2,94 +2,93 @@ import React, { Component } from 'react';
 import { withAPI } from '@winwin/api-firebase';
 import { withRouter } from 'react-router-dom';
 
-
 interface LoginState {
-    email: string;
-    password: string;
-    persistAuth: boolean;
-    error: Error | undefined
+  email: string;
+  password: string;
+  persistAuth: boolean;
+  error: Error | undefined;
 }
 
 const INITIAL_STATE: LoginState = {
-    email: '',
-    password: '',
-    persistAuth: false,
-    error: undefined,
+  email: '',
+  password: '',
+  persistAuth: false,
+  error: undefined,
 };
 
 export class Login extends Component<any, LoginState> {
-    constructor(props) {
-        super(props);
-        this.state = INITIAL_STATE;
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = INITIAL_STATE;
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    handleChange(e){
-        e.persist();
-        if (Object.keys(this.state).includes(e.target.name)) {
-            const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
-            this.setState(function (prevState, props) {
-                return {
-                    [e.target.name]: value
-                } as Pick<LoginState, keyof LoginState>
+  handleChange(e) {
+    e.persist();
+    if (Object.keys(this.state).includes(e.target.name)) {
+      const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
+      this.setState((prevState, props) => {
+        return {
+          [e.target.name]: value,
+        } as Pick<LoginState, keyof LoginState>;
+      });
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    return this.props.api.auth.login.setPersistence(this.state.persistAuth ? 'local' : 'session')
+      .then(() => {
+        return this.props.api.auth.login.signIn(this.state.email, this.state.password)
+          .then((userCredential) => {
+            if (userCredential.user.emailVerified) {
+              this.props.history.push('/dashboard');
+            } else {
+              this.props.history.push('/emailverificationrequired');
+            }
+          }).catch((error) => {
+            this.setState(() => {
+              return { error };
             });
-        };
-    };
+            alert(error.message);
+          });
+      });
+  }
 
-    handleSubmit(e) {
-        e.preventDefault();
-        return this.props.api.auth.login.setPersistence(this.state.persistAuth ? 'local' : 'session').then(() => {
-            return this.props.api.auth.login.signIn(this.state.email, this.state.password)
-                .then((userCredential) => {
-                    if (userCredential.user.emailVerified) {
-                        this.props.history.push('/dashboard');
-                    } else {
-                        this.props.history.push('/emailverificationrequired');
-                    }
-                }).catch((error) => {
-                    this.setState(function(){
-                        return {error}
-                    });
-                    alert(error.message)
-                });
-        });
-    }
+  render() {
+    return (
+      <div>
+          <h1> Login Page </h1>
+          <form onSubmit={this.handleSubmit}>
 
+              <label htmlFor="email">Email: </label> <br/>
+              <input name="email" id="email"
+                     value={this.state.email}
+                     onChange={this.handleChange}
+                     type="email"
+                     placeholder="Email Address"/><br/>
 
-    render() {
-        return (
-            <div>
-                <h1> Login Page </h1>
-                <form onSubmit={this.handleSubmit}>
+              <label htmlFor="password">Password: </label><br/>
+              <input name="password"
+                     id="password"
+                     value={this.state.password}
+                     onChange={this.handleChange}
+                     type="password"
+                     placeholder="Password"/><br/>
+              Stay Logged In?
+              <input
+                  name="persistAuth"
+                  type="checkbox"
+                  onChange={this.handleChange} /><br/>
 
-                    <label htmlFor="email">Email: </label> <br/>
-                    <input name="email" id="email"
-                           value={this.state.email}
-                           onChange={this.handleChange}
-                           type="email"
-                           placeholder="Email Address"/><br/>
-
-                    <label htmlFor="password">Password: </label><br/>
-                    <input name="password"
-                           id="password"
-                           value={this.state.password}
-                           onChange={this.handleChange}
-                           type="password"
-                           placeholder="Password"/><br/>
-                    Stay Logged In?
-                    <input
-                        name="persistAuth"
-                        type="checkbox"
-                        onChange={this.handleChange} /><br/>
-
-                    <input id={'submitButton'}
-                           type="submit"
-                           value={'Log In'}></input>
-                </form>
-            </div>
-        );
-    };
+              <input id={'submitButton'}
+                     type="submit"
+                     value={'Log In'} />
+          </form>
+      </div>
+    );
+  }
 }
 
-export default withAPI(withRouter(Login))
+export default withAPI(withRouter(Login));
