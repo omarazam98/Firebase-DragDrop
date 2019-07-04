dfa5aa725235a8d47e5e2d3da433f49e37689c5f
Julia <julia.st.jean@winwinhomesharing.com>
2019-07-04 00:01:04 -0400

    Small formatting changes

--- src/components/PaymentCard/index.tsx
+++ src/components/PaymentCard/index.tsx
@@ -127,26 +127,26 @@ import './index.css';
 import { withAPI } from '@winwin/api-firebase';
 
 interface PaymentMethods {
-  type: string,
-  number: number,
-  main: boolean,
+  type: string;
+  number: number;
 }
 
 interface UserType {
-  displayName: string,
-  uid: string,
+  displayName: string;
+  uid: string;
 }
 
 interface PaymentState {
-  paymentMethods: PaymentMethods[] | undefined;
+  paymentMethod: PaymentMethods | undefined;
   user: UserType | undefined;
 }
 const INITIAL_STATE = {
-  paymentMethods: undefined,
+  paymentMethod: undefined,
   user: undefined,
-}
-export class PaymentCard extends React.Component<any,PaymentState>{
-  constructor(props){
+};
+
+export class PaymentCard extends React.Component<any, PaymentState>{
+  constructor(props) {
     super(props);
     this.state = INITIAL_STATE;
   }
@@ -157,33 +157,29 @@ export class PaymentCard extends React.Component<any,PaymentState>{
       this.props.api.data.users.getPaymentMethods(user.uid).then((payments) => {
         this.setState(() => {
           return {
-            paymentMethods: payments,
             user,
-          }
-        })
+            paymentMethod: payments,
+          };
+        });
       });
     }
   }
 
-  render(){
-    let mainPayment;
-    if(this.state.paymentMethods) {
-      mainPayment = this.state.paymentMethods.find(x => x.main);
-    }
+  render() {
     return(
-      <div className='credit-card'>
-        <div className='credit-card__logo'>Win-Win Payment Method</div>
-        <div className='credit-card__number'>
-          {mainPayment && "XXXX XXXX XXXX " + mainPayment.number}
+      <div className="credit-card">
+        <div className="credit-card__logo">Win-Win Payment Method</div>
+        <div className="credit-card__number">
+          {this.state.paymentMethod && 'XXXX XXXX XXXX ' + this.state.paymentMethod.number}
         </div>
 
-        <div className='credit-card__info'>
-          <div className='credit-card__info_name'>
-            <div className='credit-card__info_label'>CARDHOLDER'S NAME</div>
+        <div className="credit-card__info">
+          <div className="credit-card__info_name">
+            <div className="credit-card__info_label">CARDHOLDER'S NAME</div>
             <div>{this.state.user && this.state.user.displayName}</div>
           </div>
 
-          <div className='credit-card__info_expiry'>
+          <div className="credit-card__info_expiry">
             <button>Change</button>
           </div>
         </div>
