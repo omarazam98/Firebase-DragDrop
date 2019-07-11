57870d18aa3420fc102d2f26c6767e3da317dc21
Julia <julia.st.jean@winwinhomesharing.com>
2019-07-11 16:38:19 -0400

    Fixed conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import './styles/App.css';
 import { Provider } from 'react-redux';
 import { store } from './store/createStore';
 import Navigation from './components/Navigation';
-<<<<<<< refs/remotes/origin/integration
 import { APIProvider } from '@winwin/api-firebase';
 
 const firebaseConfig = {
@@ -14,41 +13,19 @@ const firebaseConfig = {
   storageBucket: window['_env_'].REACT_APP_STORAGE_BUCKET,
   messagingSenderId: window['_env_'].REACT_APP_MESSAGING_SENDER_ID,
   appId: window['_env_'].REACT_APP_APP_ID,
-=======
-import {APIProvider} from '@winwin/api-firebase';
-
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
->>>>>>> Added calls to API for payment info
-};
+}
 
 class App extends Component {
-
   render() {
     return (
       // The provider allows all of its descendant elements to access the redux store through
       // the connect function from react-redux
-<<<<<<< refs/remotes/origin/integration
       <Provider store={store}>
         <APIProvider config={firebaseConfig}>
           <Navigation />
           { /*Put the main application in here. Probably the app router*/ }
         </APIProvider>
       </Provider>
-=======
-      <APIProvider config={firebaseConfig}>
-        <Provider store={store}>
-          <Navigation/>
-          { /*Put the main application in here. Probably the app router*/}
-        </Provider>
-      </APIProvider>
->>>>>>> Added calls to API for payment info
     );
   }
 }
