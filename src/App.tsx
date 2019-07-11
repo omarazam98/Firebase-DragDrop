c0ca506b8bbc354ec741b530902adde502553fe6
Brendan Lucas <brendanwflucas@gmail.com>
2019-07-11 11:13:37 -0400

    Remove mistaken process.env

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,6 @@ import Navigation from './components/Navigation';
 import { APIProvider } from '@winwin/api-firebase';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-};
-/*
-const firebaseConfig = {
   apiKey: window['_env_'].REACT_APP_API_KEY,
   authDomain: window['_env_'].REACT_APP_AUTH_DOMAIN,
   databaseURL: window['_env_'].REACT_APP_DATABASE_URL,
@@ -24,7 +14,7 @@ const firebaseConfig = {
   messagingSenderId: window['_env_'].REACT_APP_MESSAGING_SENDER_ID,
   appId: window['_env_'].REACT_APP_APP_ID,
 };
-*/
+
 class App extends Component {
 
   render() {
