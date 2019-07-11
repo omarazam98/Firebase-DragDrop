9734c650b4053532de4ffa374c1741647358df31
Julia <julia.st.jean@winwinhomesharing.com>
2019-07-11 16:13:35 -0400

    fixed tests

--- src/tests/components/PaymentCard/PaymentCard.test.tsx
+++ src/tests/components/PaymentCard/PaymentCard.test.tsx
@@ -1,21 +1,37 @@
-import { PaymentCard, styles } from '../../../components/PaymentCard';
 import * as React from 'react';
-import { shallow, render, mount } from 'enzyme';
+import { PaymentCard, styles } from '../../../components/PaymentCard';
+import { shallow } from 'enzyme';
 
-const mockAPI = {
-  auth: {
-    signOut: jest.fn(),
-    currentUser: jest.fn(),
+const mockState = {
+  paymentMethod: {
+    type: 'visa',
+    number: 1010,
+  },
+  user: {
+    uid: '1111',
+    displayName: 'The Weapon',
   },
 };
 
-const options = {
-  lifecycleExperimental: false,
-  disableLifecycleMethods: true,
+const mockAPI = {
+  auth: {
+    currentUser: () => {
+      jest.fn();
+    },
+  },
 };
 
-test('Render the payment card', () => {
+test('Render the payment card with no data', () => {
   const wrapper = shallow(<PaymentCard classes={styles} api={mockAPI}/>);
   expect(wrapper).toMatchSnapshot();
-  console.log(wrapper.dive().debug())
+});
+
+test('Render full payment card with data', () => {
+  const wrapper = shallow(<PaymentCard classes={styles} api={mockAPI}/>);
+  wrapper.setState(mockState);
+  expect(wrapper).toMatchSnapshot();
+  expect(wrapper.find('#userName').text())
+    .toBe(mockState.user.displayName);
+  expect(wrapper.find('#payment').text())
+    .toBe(`XXXX XXXX XXXX ${mockState.paymentMethod.number}`);
 });
